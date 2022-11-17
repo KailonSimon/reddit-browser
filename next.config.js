5cfeaa3a7aae73f6e6d3b15b3b18a93ae6616761
Kailon Simon <81045491+KailonSimon@users.noreply.github.com>
2022-11-17 05:16:01 -0600

    Add redirects for reddit internal links

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/r/:slug",
+        destination: "/sub/:slug",
+        permanent: true,
+      },
+      {
+        source: "/u/:slug",
+        destination: "/user/:slug",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
